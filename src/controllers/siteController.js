12ce5fde7d5217093e3dc1252cde11ed4f9a4d67
tuansienhan2000 <trananhtuan.kg2000@gmail.com>
2021-12-12 10:54:47 +0700

    edit sort

--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -1,71 +1,71 @@
-const Product = require("./models/products")
+const Product = require("./models/products");
 const { multipleMongooseToObject } = require("../util/mongoose");
-
 
 class siteController {
     // [GET] /
-    error(req, res, next){
-        res.render('404')
-     }
+    error(req, res, next) {
+        res.render("404");
+    }
     index(req, res, next) {
-        Product
-            .find({deletedAt:null})
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
-
+        Product.find({ deletedAt: null })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
     sortByNameAZ(req, res, next) {
-        Product
-            .find({deletedAt:null}).sort( { name: 1 } )
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
-
+        Product.find({ deletedAt: null })
+            .sort({ name: 1 })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
     sortByNameZA(req, res, next) {
-        Product
-            .find({deletedAt:null}).sort( { name: -1 } )
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
-
+        Product.find({ deletedAt: null })
+            .sort({ vn_name: 1 })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
     sortByYearIncs(req, res, next) {
-        Product
-            .find({deletedAt:null}).sort( { year: 1 } )
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
-
+        Product.find({ deletedAt: null })
+            .sort({ ending: -1 })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
     sortByYearDec(req, res, next) {
-        Product
-            .find({deletedAt:null}).sort( { year: -1 } )
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
-
+        Product.find({ deletedAt: null })
+            .sort({ year: -1 })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
 
     // [GET] /search
     search(req, res, next) {
-        Product
-            .find({ deletedAt:null, name: new RegExp(req.query.q, 'i') })
-            .then(products => res.render('main', {
-                products: multipleMongooseToObject(products)
-            }))
-            .catch(next)
+        Product.find({ deletedAt: null, name: new RegExp(req.query.q, "i") })
+            .then((products) =>
+                res.render("main", {
+                    products: multipleMongooseToObject(products),
+                })
+            )
+            .catch(next);
     }
-
- 
-
 }
 
 module.exports = new siteController();
-
