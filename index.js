3e50b1adfce8a4e0dfa5346a947c104d80c02340
tuansienhan2000 <trananhtuan.kg2000@gmail.com>
2021-12-13 21:17:52 +0700

    add icon

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const express = require('express')
+const express = require("express");
 const path = require("path");
-const app = express()
-var session = require('express-session');
+const app = express();
+var session = require("express-session");
 const methordOverride = require("method-override");
-const route = require('./src/routes')
-const data = require('./src/config/connect_db')
+const route = require("./src/routes");
+const data = require("./src/config/connect_db");
+var favicon = require("serve-favicon");
 const port = process.env.PORT || 3000;
-
+app.use(favicon("./src/public/img/pngwing.com.png"));
 app.use(express.static(path.join(__dirname, "src/public")));
 app.use(express.json());
 app.use(methordOverride("_method"));
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
-app.use(session({
-  resave: true, 
-  saveUninitialized: true, 
-  secret: 'somesecret', 
-  cookie: { maxAge: 1000*60*60 }}));
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "somesecret",
+        cookie: { maxAge: 1000 * 60 * 60 },
+    })
+);
 
 data.connect();
-app.get('/donggop', function (req, res) {
-  res.render('donate')
-})
-app.get('/nguon', function (req, res) {
-  res.render('trangnguon')
-})
-route(app)
-
+app.get("/donggop", function (req, res) {
+    res.render("donate");
+});
+app.get("/nguon", function (req, res) {
+    res.render("trangnguon");
+});
+route(app);
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src/views" ));
-
+app.set("views", path.join(__dirname, "src/views"));
 
 //route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-  });
+});
